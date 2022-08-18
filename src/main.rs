5be5b6b568939ed903e1c8a5ee6e7bec963e6084
mizsho <34414292+mizsho@users.noreply.github.com>
2022-08-19 00:46:13 +0900

    recursive_gcd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 
 mod exhaustive_search;
+mod recursive_gcd;
 
 fn main() {
-    exhaustive_search::exhaustive_search();
+
+    // exhaustive_search::exhaustive_search();
+    recursive_gcd::exec();
+
 }
---
+++ src/recursive_gcd.rs
@@ -0,0 +1,28 @@
+
+use proconio::input;
+use std::time::{Duration, Instant};
+
+pub fn exec() {
+
+    println!("Input two numbers m, n (0~65535).");
+    input! {
+        m: u16,
+        n: u16,
+    }
+
+    println!("gcd({}, {}) = {}", m, n, gcd(m, n))
+
+}
+
+pub fn gcd(m: u16, n: u16) -> u16 {
+
+    // recursively calc GCD by euclidean algorithm.
+    // GCD(m, n) = GCD(n, r), r = m%n
+    
+    if n==0 {
+        return m;
+    } else {
+        return gcd(n, m%n)
+    }
+
+}
