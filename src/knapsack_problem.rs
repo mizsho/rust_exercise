7b4c024e0b447ae503ae19a91c9363ba2d4fe3a1
mizsho <34414292+mizsho@users.noreply.github.com>
2022-08-20 00:02:23 +0900

    minor fix

--- src/knapsack_problem.rs
+++ src/knapsack_problem.rs
@@ -15,8 +15,8 @@ pub fn exec() {
     const N: usize = 10;
     const WMAX: usize = 50;
 
-    let mut arr_w = vec![0i32; N];
-    let mut arr_v = vec![0i32; N];
+    let mut arr_w = vec![0usize; N];
+    let mut arr_v = vec![0usize; N];
     for i in 0..N {
         arr_w[i] = rand::thread_rng().gen_range(1..=100);
         arr_v[i] = rand::thread_rng().gen_range(1..=100);
@@ -28,21 +28,20 @@ pub fn exec() {
     //     - total weight <= w
     //     - pick first i items    
     let mut dp = vec![
-        vec![0i32; WMAX+1];
+        vec![0usize; WMAX+1];
         N+1
     ];
 
-    for iu in 1..=N {
-        for wu in 0..=WMAX {
-            let w = wu as i32;
+    for i in 1..=N {
+        for w in 0..=WMAX {
             // ith item is picked
-            if w - arr_w[iu-1] > 0 {
-                let b = dp[iu-1][(w - arr_w[iu-1]) as usize] + arr_v[iu-1];
-                chmax(&mut dp[iu][wu], b);
+            if w > arr_w[i-1] {
+                let b = dp[i-1][w - arr_w[i-1]] + arr_v[i-1];
+                chmax(&mut dp[i][w], b);
             }
             // ith item is not picked
-            let b = dp[iu-1][wu];
-            chmax(&mut dp[iu][wu], b);            
+            let b = dp[i-1][w];
+            chmax(&mut dp[i][w], b);            
         }
     }
 
@@ -50,7 +49,7 @@ pub fn exec() {
     println!("dp array: {:?}", dp);
 }
 
-fn chmax(a: &mut i32, b: i32) {
+fn chmax(a: &mut usize, b: usize) {
 
     // choose maximum value function for DP.
     // a: vector element (mutable reference)
