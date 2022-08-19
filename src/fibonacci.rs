227dd82132e3d7d69233576ecfc663f5ae14984a
mizsho <34414292+mizsho@users.noreply.github.com>
2022-08-19 16:09:55 +0900

    fibonacci (cache)

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -4,29 +4,68 @@ use std::time::{Duration, Instant};
 
 pub fn exec() {
 
-    println!("Input a number n (0~255).");
+    // calc fibonacci sequence.
+    // fib(0) = 0
+    // fib(1) = 1
+    // fib(n) = fib(n-1) + fib(n-2)
+
+    println!("Input a number n (0~50). Larger number will cause overflow.");
     input! {
         n: u8,
     }
 
     let start = Instant::now();
-    println!("fib({}) = {}", n, fib_recursive(n));
+
+    // (1) recursive version
+    // println!("fib({}) = {}", n, fib_recursive(n));
+
+    // (2) memo version
+    println!("fib({}) = {}", n, fib_cache_exec(n));
+
     let duration = start.elapsed();
     println!("elapsed time: {:#?}", duration);
 
 }
 
-pub fn fib_recursive(n: u8) -> u32 {
+pub fn fib_recursive(n: u8) -> u64 {
 
     // recursively calc Fibonacci sequence.
-    // fib(0) = 0
-    // fib(1) = 1
-    // fib(n) = fib(n-1) + fib(n-2)
     
     match n {
         0 => 0,
         1 => 1,
         n => fib_recursive(n-1) + fib_recursive(n-2),
+    }
+
+}
+
+pub fn fib_cache_exec(n: u8) -> i64 {
+
+    // recursively calc Fibonacci sequence.
+    // use cache to reduce time.
+    
+    let n = n as usize;
+
+    // initialize memo array as -1.
+    let mut memo = [-1i64; 255];
+
+    // pass mutable reference
+    fib_cache(&mut memo, n)
+
+}
+
+fn fib_cache(memo: &mut [i64], n: usize) -> i64 {
+
+    match n {
+        0 => 0,
+        1 => 1,
+        n => {
+            if memo[n] == -1 {
+                // &mut is not needed because memo is already mutable reference here.
+                memo[n] = fib_cache(memo, n-1) + fib_cache(memo, n-2);
+            }
+            memo[n]            
+        }
     }
 
 }
