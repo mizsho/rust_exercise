d71bc90c2fefb7a7d838181df5287e9448659fb4
mizsho <34414292+mizsho@users.noreply.github.com>
2022-08-19 18:04:12 +0900

    frog problem (DP)

---
+++ src/frog_problem.rs
@@ -0,0 +1,42 @@
+
+use std::cmp;
+use rand::Rng;
+
+const N: usize = 10;
+
+pub fn exec() {
+
+    // solve frog problem.
+    // arr: height array for stages. 
+
+    // initialize array with random numbers
+    let mut arr = vec![0i32; N];
+    for i in 0..N {
+        arr[i] = rand::thread_rng().gen_range(1..=127);
+    }
+    println!("random array: {:?}", arr);
+
+    println!("{}", frog_dp(&mut arr));
+
+}
+
+pub fn frog_dp(arr: &mut Vec<i32>) -> i32 {
+
+    // solve frog problem by DP.
+    
+    const INF: i32 = 10000;
+    let mut dp = vec![INF; N];
+    
+    dp[0] = 0;
+    dp[1] = (arr[1] - arr[0]).abs();
+
+    for i in 2..N {
+        dp[i] = cmp::min(
+            dp[i-2] + (arr[i] - arr[i-2]).abs(),
+            dp[i-1] + (arr[i] - arr[i-1]).abs(),
+        )
+    }
+
+    dp[N-1]
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 
-mod exhaustive_search;
-mod gcd;
-mod fibonacci;
+// mod exhaustive_search;
+// mod gcd;
+// mod fibonacci;
+mod frog_problem;
 
 fn main() {
 
     // exhaustive_search::exhaustive_search();
     // gcd::exec();
-    fibonacci::exec();
+    // fibonacci::exec();
+    frog_problem::exec();
 
 }
