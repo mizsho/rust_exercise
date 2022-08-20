d731a2b1e8a57a9e5267f03e19923cb2df265e81
mizsho <34414292+mizsho@users.noreply.github.com>
2022-08-20 15:46:28 +0900

    add "edit distance"

---
+++ src/edit_distance.rs
@@ -0,0 +1,72 @@
+
+use proconio::input;
+
+pub fn exec() {
+
+    // Calc the edit distance between two Strings s,t.
+    // The following edit operations are allowed:
+    // (1) change (a letter of s)
+    // (2) delete (a letter of s)
+    // (3) insert (a letter in s)
+    // Insert is equivalent as delete a letter of t.
+
+    println!("input two strings:");
+    input! {
+        s: String,
+        t: String,
+    }
+
+    // dp[i][j]: the edit distance of
+    // first i letters of s and first j letters of t.
+    const INF: usize = 10000;
+    let mut dp = vec![
+        vec![INF; t.len()+1];
+        s.len()+1
+    ];
+
+    // initial condition
+    dp[0][0] = 0;
+
+    for i in 0..=s.len() {
+        for j in 0..=t.len() {
+            // (1) change
+            if (i > 0) && (j > 0) {
+                // index should be i-1 to pick ith char.
+                if s.chars().nth(i-1) != t.chars().nth(j-1) {
+                    let b = dp[i-1][j-1] + 1;
+                    chmin(&mut dp[i][j], b);
+                } else {
+                    let b = dp[i-1][j-1];
+                    chmin(&mut dp[i][j], b);
+                }
+            }
+
+            // (2) delete a letter of s
+            if i > 0 {
+                let b = dp[i-1][j] + 1;
+                chmin(&mut dp[i][j], b);
+            }
+
+            // (3) insert: delete a letter of t
+            if j > 0 {
+                let b = dp[i][j-1] + 1;
+                chmin(&mut dp[i][j], b);
+            }
+        }
+    }
+
+    println!("edit distance: {}", dp[s.len()][t.len()]);
+    println!("dp array: {:?}", dp);
+}
+
+fn chmin(a: &mut usize, b: usize) {
+
+    // choose minimum value function for DP.
+    // a: vector element (mutable reference)
+    // b: reference number
+    
+    if *a > b {
+        *a = b;
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 // mod gcd;
 // mod fibonacci;
 // mod frog_problem;
-mod knapsack_problem;
+// mod knapsack_problem;
+mod edit_distance;
 
 fn main() {
 
@@ -11,6 +12,7 @@ fn main() {
     // gcd::exec();
     // fibonacci::exec();
     // frog_problem::exec();
-    knapsack_problem::exec();
+    // knapsack_problem::exec();
+    edit_distance::exec();
 
 }
