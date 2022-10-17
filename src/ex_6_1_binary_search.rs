382a56a0b2b7b09d5625d21c5be607abc1419c32
mizsho <34414292+mizsho@users.noreply.github.com>
2022-10-17 23:12:27 +0900

    add ex6_1

---
+++ src/ex_6_1_binary_search.rs
@@ -0,0 +1,33 @@
+use proconio::input;
+
+pub fn exec() {
+
+    println!("Binary search.");
+    println!("input the length of your array:");
+    input! {
+        n: usize,
+    }
+    println!("input an integer sorted array (e.g. 1 2 5 10 21):");
+    input! {
+        a: [i32; n],
+    }
+    println!("input an integer you look for:");
+    input! {
+        k: i32,
+    }
+
+    let mut left = 0;
+    let mut right = n - 1;
+    while left <= right {
+        let mut mid = (left + right) / 2;
+        if a[mid as usize]==k {
+            println!("anwer: {}", mid);
+            break
+        } else if a[mid as usize] > k {
+            right = mid - 1;
+        } else if a[mid as usize] < k {
+            left = mid + 1;
+        }
+    }
+
+}
